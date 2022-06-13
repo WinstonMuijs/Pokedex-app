8f16338651336673c5a72b0e25535b8f54dd83c3
Winston Muijs <winstonhmuijs@gmail.com>
2022-06-13 11:49:59 +0200

    Updated code in script.js
    Added a few objects and list. List contains pokemon objects with different properties. loop creates three new lists with names, types and height properties of the diff. objects and displays it on index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
-alert('Hello,World');
 
-let myName = 'Hi! My Name is Winston Muijs and i\'m a webdeveloper from the Netherlands and my favorite food is...';
-document.write(myName);
+const pokemon1 = {name: 'Palkia', type: ['Dragon', 'Water'], height:4.2};
+const pokemon2 = {name: 'Rapidash', type: ['Fire'], height:1.7};
+const pokemon3= {name: 'Venusaur', type: ['Grass', 'Poisson'], height:2};
 
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+
+
+const pokemonList = [
+  pokemon1, pokemon2, pokemon3
+];
+
+let names = []
+let types = []
+let heights = []
+
+for (let i=0; i < pokemonList.length; i++) {
+   names += pokemonList[i].name + "\n";
+   types += pokemonList[i].type  + "\n";
+   heights += pokemonList[i].height + "\n";
+}
+
+document.write(names);
+document.write(types);
+document.write(heights);
