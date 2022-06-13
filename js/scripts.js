5ddb004ba724c060d177805079e0fd4a8ae4cb70
Winston Muijs <winstonhmuijs@gmail.com>
2022-06-13 07:59:38 +0200

    Added a scripts.js to js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert('Hello,World');
+
+let myName = 'Hi! My Name is Winston Muijs and i\'m a webdeveloper from the Netherlands and my favorite food is...';
+document.write(myName);
+
+let favoriteFood = 'Pasta';
+document.write(favoriteFood);
