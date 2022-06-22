c5c1fd53c7bc46ede69f01997735165e3455f20b
Winston Muijs <winstonhmuijs@gmail.com>
2022-06-22 09:46:36 +0200

    Updated scripts.js
    added functions : updated forEach loop. Added buttons, eventListener to IIFE with event function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ const pokemon1 = {name: 'Palkia', type: ['Dragon', 'Water'], height:4.2};
 const pokemon2 = {name: 'Rapidash', type: ['Fire'], height:1.7};
 const pokemon3= {name: 'Venusaur', type: ['Grass', 'Poisson'], height:2};
 
+// array of variables (pokemons)
 
-// array of variables (pokemons) in IIFE and with add and getAll function
+
+
 let pokemonRepository = (function () {
   let pokemonList = [pokemon1, pokemon2, pokemon3];
-// add function: adds pokemon to the list with typeof control before adding.
+
   function add(pokemon) {
     if(typeof pokemon === 'object'){
       pokemonList.push(pokemon);
@@ -15,29 +17,46 @@ let pokemonRepository = (function () {
       document.write("It's not a Pokemon");
     }
   }
-//getAll function: get all pokemon on the List
+
   function getAll() {
     return pokemonList;
   }
 
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
+
+  function addEvent(button, pokemon){
+    button.addEventListener('click', function(){
+      showDetails(pokemon);
+    });
+
+
+  }
+
+  function addListItems(pokemon){
+    let pokemonNewList = document.querySelector('ul');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('btn');
+    listItem.appendChild(button);
+    pokemonNewList.appendChild(listItem);
+    addEvent(button, pokemon);
+  }
+
   return {
     add: add,
-    getAll: getAll
+    addListItems: addListItems,
+    getAll: getAll,
+    showDetails: showDetails
+
   };
 })();
 
-//forEach function to get all pokemons on the list, checks the keys and write them to the html document.
+
+
+
 pokemonRepository.getAll().forEach(function(pokemon){
-  document.write("<hr>");
-  Object.keys(pokemon).forEach(function(property){
-    if(property === 'name' && pokemon[property] !=''){
-      document.write("Name is: " + "<h3 style='color:yellow;'>" + pokemon[property] + "</h3>");
-    }else if(property === 'type' && pokemon[property] != []){
-      document.write("<p style ='color:lightblue;'>" + "Type is: " + pokemon[property] + "</p>");
-    }else if(property === 'height' && pokemon[property] > 0){
-      document.write("<p style = 'color:lightgreen;'>"+ "Height is: " + pokemon[property] + "</p>");
-    }else{
-      document.write("Check your properties!")
-    }
-  });
+  pokemonRepository.addListItems(pokemon);
 });
