cfce8095b3d1b8e92d307c602dd3da57d79e0636
Winston Muijs <winstonhmuijs@gmail.com>
2022-06-22 13:13:52 +0200

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,29 @@ const pokemon1 = {name: 'Palkia', type: ['Dragon', 'Water'], height:4.2};
 const pokemon2 = {name: 'Rapidash', type: ['Fire'], height:1.7};
 const pokemon3= {name: 'Venusaur', type: ['Grass', 'Poisson'], height:2};
 
-// array of variables (pokemons)
 
-
-
+// IIFE for the pokemonList and it's functions
 let pokemonRepository = (function () {
   let pokemonList = [pokemon1, pokemon2, pokemon3];
 
+// add pokemon object to the list
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if(typeof pokemon === 'object'){
+      pokemonList.push(pokemon);
+    }else{
+      console.log("It's not a Pokemon");
+    }
   }
 
+// get all pokemon in the list
   function getAll() {
     return pokemonList;
   }
-
+// shows details of the pokemon
   function showDetails(pokemon){
     console.log(pokemon);
   }
-
+// eventListener for the click on the button
   function addEvent(button, pokemon){
     button.addEventListener('click', function(){
       showDetails(pokemon);
@@ -29,7 +33,7 @@ let pokemonRepository = (function () {
 
 
   }
-
+// adds List items
   function addListItems(pokemon){
     let pokemonNewList = document.querySelector('ul');
     let listItem = document.createElement('li');
@@ -40,7 +44,7 @@ let pokemonRepository = (function () {
     pokemonNewList.appendChild(listItem);
     addEvent(button, pokemon);
   }
-
+// return global accessible functions
   return {
     add: add,
     addListItems: addListItems,
@@ -52,7 +56,8 @@ let pokemonRepository = (function () {
 
 
 
-
+//get all pokemon in the list and for each pokemon
 pokemonRepository.getAll().forEach(function(pokemon){
+  //adds items to the pokemon
   pokemonRepository.addListItems(pokemon);
 });
