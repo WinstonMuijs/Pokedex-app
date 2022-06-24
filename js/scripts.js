15af6ec82f5d331d34b17a6eaad9b3fc68cb61aa
Winston Muijs <winstonhmuijs@gmail.com>
2022-06-24 19:51:24 +0200

    Updated the scripts.js file
    Added a loadList and LoadDetails promise functions to the IIFE to fetch data from the pokeapi.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
-// variables with objects as values
-const pokemon1 = {name: 'Palkia', type: ['Dragon', 'Water'], height:4.2};
-const pokemon2 = {name: 'Rapidash', type: ['Fire'], height:1.7};
-const pokemon3= {name: 'Venusaur', type: ['Grass', 'Poisson'], height:2};
-
-
-// IIFE for the pokemonList and it's functions
 let pokemonRepository = (function () {
-  let pokemonList = [pokemon1, pokemon2, pokemon3];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-// add pokemon object to the list
   function add(pokemon) {
     if(typeof pokemon === 'object'){
       pokemonList.push(pokemon);
@@ -17,23 +10,20 @@ let pokemonRepository = (function () {
     }
   }
 
-// get all pokemon in the list
   function getAll() {
     return pokemonList;
   }
-// shows details of the pokemon
+
   function showDetails(pokemon){
     console.log(pokemon);
   }
-// eventListener for the click on the button
+
   function addEvent(button, pokemon){
     button.addEventListener('click', function(){
       showDetails(pokemon);
     });
-
-
   }
-// adds List items
+
   function addListItems(pokemon){
     let pokemonNewList = document.querySelector('ul');
     let listItem = document.createElement('li');
@@ -44,20 +34,39 @@ let pokemonRepository = (function () {
     pokemonNewList.appendChild(listItem);
     addEvent(button, pokemon);
   }
-// return global accessible functions
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+
   return {
     add: add,
     addListItems: addListItems,
     getAll: getAll,
-    showDetails: showDetails
+    showDetails: showDetails,
+    loadList: loadList
+
 
   };
 })();
 
 
 
-//get all pokemon in the list and for each pokemon
-pokemonRepository.getAll().forEach(function(pokemon){
-  //adds items to the pokemon
-  pokemonRepository.addListItems(pokemon);
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItems(pokemon);
+  });
 });
